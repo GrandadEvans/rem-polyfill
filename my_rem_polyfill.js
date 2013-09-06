7fe8796afe3487198e4cd12eed13f69bb8f7cacb
John Evans <john@grandadevans.com>
2013-09-06 11:10:05 +0100

    Updated the file with a few more checks etc. Next I need to take the jQuery get out and replace it with native JS alternative

--- my_rem_polyfill.js
+++ my_rem_polyfill.js
@@ -1,116 +1,157 @@
 (function (window) {
 	
-	// Get the base font-size
-	var x, z;
-	var base_font_size = getFontSize();
-	var array_to_push = [];
+	// Set all the variables we will be using in this script
+	var x, z,
+        base_font_size = getFontSize()
+        array_to_push = [],
+        styles_arr;,
+        href,
+        current_index,
+        current_href,
+        old_length,
+        lines,
+        clean_array,
+        clean_lines,
+        line,
+        prop,
+        val,
+        new_font-size,
+        str_replace_all,
+        int_index_of_match,
+        new_length;
 	
-	log('Font size is: ' + base_font_size);
+	log('Base font size is: ' + base_font_size);
 	
 	// go through all of the stylesheets
-	var styles_arr = document.styleSheets;
+	styles_arr = document.styleSheets;
 	
+    // Make sure that we have styleets available
+    if (styles_arr.length === 0) {
+        log("There are no stylesheets to use :-(");
+        return false;
+    }
+
 	for (z=0; z<document.styleSheets.length; z++) {
-		// We only want the combines version from Lemonstand
+		// We only want the Lemonstand combines version from Lemonstand
 		if (document.styleSheets[z].href.indexOf('ls_css_combine') >= 0) {
 			
-			var href = document.styleSheets[z].href;
+			href = document.styleSheets[z].href;
 			
-			var currentIndex = z;
-			var currentHref = href;
+			current_index = z;
+			current_href = href;
 			
 			$.get(href, function(styles) {
 				
-				var currentHref = href;
-				var old_length = styles.length;
+				current_href = href;
+				old_length = styles.length;
 
 				// split the string into lines
-				var lines = split_lines(styles);
+				lines = split_lines(styles);
 				
 				log('There are ' + lines.length + ' lines in stylesheet ' + z);
 			
+                // Make sure there are lines to test in the stylesheet
+                if (lines.length === 0) {
+                    log("There are no lines to test in this stylesheet :-(");
+                    return false;
+                }
+
 				// Iterate through the lines in the stylesheets
 				for (a = 0; a < lines.length; a++) {
 						
 					// split lines with multiple rules into separate lines.
-					var clean_array = split_rules(lines[a]);
+					clean_array = split_rules(lines[a]);
 					
 					// Now that we have the split rules we again need to split them into lines
-					var clean_lines = split_lines(clean_array);
+					clean_lines = split_lines(clean_array);
 					
 					// Now that we definitely have individual lines we can perform the search and replace
 					for (x=0; x < clean_lines.length; x++) {
 						// Don't waste our time converting irrelevant lines
 						if (clean_lines[x].indexOf('}') == -1 && clean_lines[x].indexOf('{') == -1 && clean_lines[x].indexOf("\n") == -1) {
-							var line = clean_lines[x];
+							line = clean_lines[x];
 							
 							log('Line ' + a + '|' + x + ': ' + line);
 							
+                            // Check to make sure that the string 'rem' appears after the colon to bypass things such as "counter-increment: inherit;"
 							if (line.indexOf('rem') >= 0 && (line.indexOf('rem') > line.indexOf(':'))) {
 							
 							log(lines);
-							// parse the rem unit
 							
 							// get the property
-							var prop = line.split(':');
+							prop = line.split(':');
+
+                            // make sure we have an index of 2
 							if (prop.length === 2) {
 								prop = prop[1];
 								prop = prop.replace(';', '');
 								
-								var val = parseFloat(prop);
+								val = parseFloat(prop);
 								
-								// Now multiply the rem buy the font-size
-								var new_font_size = (val * parseFloat(base_font_size));
-								
-								new_font_size = ' ' + new_font_size + 'px';
-								
-								// replace the property stylesheet wide
-	
-								var strReplaceAll = styles;
-								var intIndexOfMatch = strReplaceAll.indexOf( prop );
-								
-								// Loop over the string value replacing out each matching
-								// substring.
-								while (intIndexOfMatch != -1){
-									// Relace out the current instance.
-									strReplaceAll = strReplaceAll.replace( prop, new_font_size)
-									
-									// Get the index of any next matching substring.
-									intIndexOfMatch = strReplaceAll.indexOf( prop );
-								}
-								 log('should have replaced ' + prop + ' with ' + new_font_size + ' in ' + currentIndex);
-								styles = strReplaceAll;
+                                // Check to make sure we have a float
+                                if (!isNaN(val)) {
+                                    // Now multiply the rem buy the font-size
+                                    new_font_size = (val * parseFloat(base_font_size));
+
+                                    new_font_size = ' ' + new_font_size + 'px';
+
+                                    // replace the property stylesheet wide
+
+                                    str_replace_all = styles;
+                                    int_index_of_match = str_replace_all.indexOf( prop );
+
+                                    // Loop over the string value replacing out each matching
+                                    // substring.
+                                    while (int_index_of_match != -1){
+                                        // Relace out the current instance.
+                                        str_replace_all = str_replace_all.replace( prop, new_font_size)
+
+                                        // Get the index of any next matching substring.
+                                        int_index_of_match = str_replace_all.indexOf( prop );
+                                    }
+
+                                    log('should have replaced ' + prop + ' with ' + new_font_size + ' in ' + current_index);
+
+                                    // Write the stylesheet contents with the replaced values back to styles
+                                    styles = str_replace_all;
+
+                                } // End of if (!isNaN check
+
 							} // end of if prop === 2
+
 						} // end of if rem is after :
+
 						} // end of converting irrenelevant lines
+
 					} //end of for clean lines
+
 				} // end of lines.length
 				
-				log('About to write to css file ' + currentIndex);
+				log('About to write to css file ' + current_index);
 				//log(styles); // output the finished result
 				
-				if (document.styleSheets[currentIndex].cssText=styles) {
-					var new_length = styles.length;
-					log('CSS file ' + currentIndex + ' old length: ' + old_length + '. New length: ' + new_length)
+                // Write the stylesheet back to the file
+				if (document.styleSheets[current_index].cssText=styles) {
+					new_length = styles.length;
+					log('CSS file ' + current_index + ' old length: ' + old_length + '. New length: ' + new_length)
 				}
 				
 			}); // End of jquery get for the stylesheet
+
 		} // end of the stylesheet iteration
 
 		// Find the index of the sylesheets
 		
 	}
+
+    }
 })(window);		
 
 function log(msg) {
 	if (console.log) {
-//		console.log(msg);
-	}
-}
 
-function dir(msg) {
-	if (console.dir) {
-		console.dir(msg);
+		// Uncomment the following line if you want to debug but make sure you have dev tools open
+//		console.log(msg);
 	}
 }
 
