5129ae62ed322510778a49346f5ec369d45da548
John Evans <john@grandadevans.com>
2013-10-03 15:26:03 +0100

    Updated script: jshint comes back clean

--- my_rem_polyfill.js
+++ my_rem_polyfill.js
@@ -4,181 +4,139 @@
  *
  */
 
-(function (window) {
-	
-	// See if the rem unit is already supported
-	if (test_support) {
+function convert_rems() {
+
+	// Make sure that we have stylesheets available and REM are not already supported
+	if (document.stylesheets.length === 0 || test_support) {
 		return false;
 	}
 
-	// Set all the variables we will be using in this script
-	var x,
-		z,
-		base_font_size = getFontSize(),
-		array_to_push = [],
-		styles_arr,
-		href,
-		current_index,
-		current_href,
-		old_length,
-		lines,
-		clean_array,
-		clean_lines,
-		line,
-		prop,
-		val,
-		new_font-size,
-		str_replace_all,
-		int_index_of_match,
-		new_length;
-	
-	// go through all of the stylesheets
-	styles_arr = document.styleSheets;
-	
-	// Make sure that we have styleets available
-	if (styles_arr.length === 0) {
-		return false;
-	}
+	for (var z = 0; z < document.styleSheets.length; z++) {
+        get_and_action_styles(document.styleSheets[z].href);
+    }
+}
 
-	for (z=0; z < document.styleSheets.length; z++) {
-		// We only want the Lemonstand combines version from Lemonstand
-		if (document.styleSheets[z].href.indexOf('ls_css_combine') >= 0) {
-			
-			href = document.styleSheets[z].href;
-			
-			current_index = z;
-			current_href = href;
-			
-			$.get(href, function(styles) {
-				
-				// Assign the href var to a different one as href will be
-				// overwritten by the time it's needed further down
-				 current_href = href;
-				old_length = styles.length;
+function get_and_action_styles(href) {
+    $.get(href, function(styles) {
 
-				// split the string into lines
-				lines = split_lines(styles);
-			
-				// Make sure there are lines to test in the stylesheet
-				if (lines.length === 0) {
-					return false;
-				}
+        // Get the base font size
+        var base_font_size = getFontSize();
 
-				// Iterate through the lines in the stylesheets
-				for (a = 0; a < lines.length; a++) {
-						
-					// split lines with multiple rules into separate lines.
-					clean_array = split_rules(lines[a]);
-					
-					// Now that we have the split rules we again need to split them into lines
-					clean_lines = split_lines(clean_array);
-					
-					// Now that we definitely have individual lines we can perform the search and replace
-					for (x=0; x < clean_lines.length; x++) {
-						// Don't waste our time converting irrelevant lines
-						if (clean_lines[x].indexOf('}') == -1 && clean_lines[x].indexOf('{') == -1 && clean_lines[x].indexOf("\n") == -1) {
-							line = clean_lines[x];
-							
-							// Check to make sure that the string 'rem' appears after the colon to bypass things such as "counter-increment: inherit;"
-							if (line.indexOf('rem') >= 0 && (line.indexOf('rem') > line.indexOf(':'))) {
-							
-							// get the property
-							prop = line.split(':');
+        var lines, clean_array, clean_lines, line, prop, val, new_font_size, str_replace_all, int_index_of_match, new_length;
 
-							// make sure we have an index of 2
-							if (prop.length === 2) {
-								prop = prop[1];
-								prop = prop.replace(';', '');
-								
-								val = parseFloat(prop);
-								
-								// Check to make sure we have a float
-								if (!isNaN(val)) {
-									// Now multiply the rem buy the font-size
-									new_font_size = (val * parseFloat(base_font_size));
+        // split the string into lines
+        lines = split_lines(styles);
 
-									new_font_size = ' ' + new_font_size + 'px';
+        // Make sure there are lines to test in the stylesheet
+        if (lines.length === 0) {
+            return false;
+        }
 
-									// replace the property stylesheet wide
+        // Iterate through the lines in the stylesheets
+        for (a = 0; a < lines.length; a++) {
 
-									str_replace_all = styles;
-									int_index_of_match = str_replace_all.indexOf( prop );
+            // split lines with multiple rules into separate lines.
+            clean_array = split_rules(lines[a]);
 
-									// Loop over the string value replacing out each matching
-									// substring.
-									while (int_index_of_match != -1){
-										// Relace out the current instance.
-										str_replace_all = str_replace_all.replace( prop, new_font_size)
+            // Now that we have the split rules we again need to split them into lines
+            clean_lines = split_lines(clean_array);
 
-										// Get the index of any next matching substring.
-										int_index_of_match = str_replace_all.indexOf( prop );
-									}
+            // Now that we definitely have individual lines we can perform the search and replace
+            for (x = 0; x < clean_lines.length; x++) {
 
-									// Write the stylesheet contents with the replaced values back to styles
-									styles = str_replace_all;
+                line = clean_lines[x];
 
-								} // End of if (!isNaN check
+                // Check to make sure that the string 'rem' appears after the colon to bypass things such as "counter-increment: inherit;"
+                if ((line.indexOf('rem') >= 0)  &&  (line.indexOf('rem') > line.indexOf(':'))) {
 
-							} // end of if prop === 2
+                    // get the property
+                    prop = line.split(':');
 
-						} // end of if rem is after :
+                    // make sure we have an index of 2
+                    if (prop.length === 2) {
+                        prop = prop[1];
+                        prop = prop.replace(';', '');
 
-						} // end of converting irrenelevant lines
+                        val = parseFloat(prop);
 
-					} //end of for clean lines
+                        // Check to make sure we have a float
+                        if (!isNaN(val)) {
+                            // Now multiply the rem buy the font-size
+                            new_font_size = (val * parseFloat(base_font_size));
 
-				} // end of lines.length
-				
-				// Write the stylesheet back to the file
-				if (document.styleSheets[current_index].cssText=styles) {
-					new_length = styles.length;
-				}
-				
-			}); // End of jquery get for the stylesheet
+                            new_font_size = ' ' + new_font_size + 'px';
 
-		} // end of the stylesheet iteration
+                            // replace the property stylesheet wide
 
-		// Find the index of the sylesheets
-		
-	}
+                            str_replace_all = styles;
+                            int_index_of_match = str_replace_all.indexOf( prop );
 
-	}
-})(window);		
+                            // Loop over the string value replacing out each matching
+                            // substring.
+                            while (int_index_of_match != -1){
+                                // Relace out the current instance.
+                                str_replace_all = str_replace_all.replace( prop, new_font_size);
+
+                                // Get the index of any next matching substring.
+                                int_index_of_match = str_replace_all.indexOf( prop );
+                            }
+
+                            // Write the stylesheet contents with the replaced values back to styles
+                            styles = str_replace_all;
+
+                        } // End of if (!isNaN check
+
+                    } // end of if prop === 2
+
+                } // end of if rem is after :
+
+            } //end of for clean lines
+
+        } // end of lines.length
+
+        // Write the stylesheet back to the file
+        document.styleSheets[current_index].cssText=styles;
+        new_length = styles.length;
+
+    }); // End of jquery get for the stylesheet
+}
 
 function split_rules(lines) {
-	return lines.replace(';', ";\n");
+    return lines.replace(';', ";\n");
 }
 
 function split_lines(lines) {
-	return lines.split("\n");
+    return lines.split("\n");
 }
 
 function getFontSize() {
-	// taken from https://github.com/chuckcarpenter/REM-unit-polyfill/blob/master/js/rem.js
-	body = document.getElementsByTagName('body')[0]
-	if (body.currentStyle) {
-		if ( body.currentStyle.fontSize.indexOf("px") >= 0 ) {
-			fontSize = body.currentStyle.fontSize.replace('px', '');
-		} else if ( body.currentStyle.fontSize.indexOf("em") >= 0 ) {
-			fontSize = body.currentStyle.fontSize.replace('em', '');
-		} else if ( body.currentStyle.fontSize.indexOf("pt") >= 0 ) {
-			fontSize = body.currentStyle.fontSize.replace('pt', '');
-		} else {
-			fontSize = (body.currentStyle.fontSize.replace('%', '') / 100) * 16; // IE8 returns the percentage while other browsers return the computed value
-		}
-	} else if (window.getComputedStyle) {
-		fontSize = document.defaultView.getComputedStyle(body, null).getPropertyValue('font-size').replace('px', ''); // find font-size in body element
-	}
-	
-	return fontSize;
+    // taken from https://github.com/chuckcarpenter/REM-unit-polyfill/blob/master/js/rem.js
+    body = document.getElementsByTagName('body')[0];
+    if (body.currentStyle) {
+        if ( body.currentStyle.fontSize.indexOf("px") >= 0 ) {
+            fontSize = body.currentStyle.fontSize.replace('px', '');
+        } else if ( body.currentStyle.fontSize.indexOf("em") >= 0 ) {
+            fontSize = body.currentStyle.fontSize.replace('em', '');
+        } else if ( body.currentStyle.fontSize.indexOf("pt") >= 0 ) {
+            fontSize = body.currentStyle.fontSize.replace('pt', '');
+        } else {
+            fontSize = (body.currentStyle.fontSize.replace('%', '') / 100) * 16; // IE8 returns the percentage while other browsers return the computed value
+        }
+    } else if (window.getComputedStyle) {
+        fontSize = document.defaultView.getComputedStyle(body, null).getPropertyValue('font-size').replace('px', ''); // find font-size in body element
+    }
+
+    return fontSize;
 }
 
-function test_support {
-	// This is takesn from the modernizr repo at
-	// https://github.com/Modernizr/Modernizr/commit/84dbc9a92d7c5c8dab1e87788f58982dab5f9f75
-	// Byhttps://github.com/segdeha
+function test_support() {
+    // This is takesn from the modernizr repo at
+    // https://github.com/Modernizr/Modernizr/commit/84dbc9a92d7c5c8dab1e87788f58982dab5f9f75
+    // Byhttps://github.com/segdeha
 
-	var elem = document.createElement('p');
-	elem.style.cssText = 'font-size: 1.5rem;';
-	return elem.style.fontSize.indexOf('rem') > -1;
+    var elem = document.createElement('p');
+    elem.style.cssText = 'font-size: 1.5rem;';
+    return elem.style.fontSize.indexOf('rem') > -1;
 }
+
+convert_rems();
